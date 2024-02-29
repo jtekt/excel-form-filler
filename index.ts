7549879c194e9c0e25ff589f074254641e788ccc
Maxime Moreillon <m.moreillon@gmail.com>
2024-02-29 12:11:29 +0900

    legacy endpoints

--- index.ts
+++ index.ts
@@ -2,11 +2,14 @@ import express from "express"
 import "express-async-errors"
 
 import cors from "cors"
-import { S3_BUCKET, S3_ENDPOINT } from "./s3"
+import { S3_BUCKET, S3_ENDPOINT, getFileList } from "./s3"
 import { version, author } from "./package.json"
 import { connect as dbConnect } from "./db"
 import excelFormsRouter from "./routes/excelForms"
 
+import { getConfigFromS3 } from "./s3"
+import { fillExcel } from "./excel"
+import { sendAttachmentByEmail } from "./mail"
 const { APP_PORT = 80 } = process.env
 
 dbConnect()
@@ -27,6 +30,30 @@ app.get("/", (req, res) => {
 })
 
 app.use("/forms", excelFormsRouter)
+
+// Legacy endpoints
+app.get("/applications", async (req, res) => {
+  const files = await getFileList(S3_BUCKET, "yml/")
+  res.send(files)
+})
+app.get("/applications/:key", async (req, res) => {
+  const { key } = req.params
+
+  const config = await getConfigFromS3(key)
+
+  res.send(config)
+})
+app.post("/applications/:key", async (req, res) => {
+  const { key } = req.params
+  const { data, email } = req.body
+
+  const config = await getConfigFromS3(key)
+  const workbook = await fillExcel(data, config)
+
+  const fileBuffer = await workbook.xlsx.writeBuffer()
+  await sendAttachmentByEmail(fileBuffer, email, config)
+  res.send("OK")
+})
 
 app.listen(APP_PORT, () => {
   console.log(`Express listening on port ${APP_PORT}`)
--- s3.ts
+++ s3.ts
@@ -1,6 +1,7 @@
 import { Client } from "minio"
 import path from "path"
 import { Response } from "express"
+import YAML from "yaml"
 
 export const {
   S3_REGION,
@@ -41,4 +42,39 @@ export const sendFormFromS3 = async (res: Response, key: any) => {
   stream.on("error", (err) => {
     res.end()
   })
+}
+
+// Legacy
+export const getFileList = (bucket: string, prefix: string) =>
+  new Promise(async (resolve, reject) => {
+    const stream = await minioClient.listObjects(bucket, prefix)
+    const objects: any[] = []
+    stream.on("data", (obj) => {
+      if (!obj.name) return
+      const { name } = path.parse(obj?.name)
+      objects.push(name)
+    })
+    stream.on("error", (err) => {
+      reject(err)
+    })
+    stream.on("end", () => {
+      resolve(objects)
+    })
+  })
+
+const stream2Buffer = (dataStream: any) =>
+  new Promise((resolve, reject) => {
+    const chunks: any = []
+    dataStream.on("data", (chunk: any) => chunks.push(chunk))
+    dataStream.on("end", () => resolve(Buffer.concat(chunks)))
+    dataStream.on("error", reject)
+  })
+
+export const getConfigFromS3 = async (key: string) => {
+  const stream = await minioClient.getObject(
+    S3_BUCKET,
+    `yml/${decodeURIComponent(key)}.yml`
+  )
+  const buffer: any = await stream2Buffer(stream)
+  return YAML.parse(buffer.toString())
 }
