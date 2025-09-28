bd42632874e77aab14a12f8f810137c78b2abe5e
Maxime Moreillon <m.moreillon@gmail.com>
2025-09-29 07:39:13 +0900

    SMTP auth

--- mail.ts
+++ mail.ts
@@ -1,35 +1,46 @@
-import nodemailer from "nodemailer"
-import { UserEmailConfig } from "./controllers/excelForms"
+import nodemailer from "nodemailer";
+import { UserEmailConfig } from "./controllers/excelForms";
 
-const { SMTP_HOST = "localhost", SMTP_PORT = "25" } = process.env
+const {
+  SMTP_HOST = "localhost",
+  SMTP_PORT = "25",
+  SMTP_USERNAME,
+  SMTP_PASSWORD,
+  SMTP_SECURE,
+} = process.env;
+
+const smtpAuth =
+  SMTP_USERNAME && SMTP_PASSWORD
+    ? {
+        user: SMTP_USERNAME,
+        pass: SMTP_PASSWORD,
+      }
+    : undefined;
 
 export const transporter = nodemailer.createTransport({
   host: SMTP_HOST,
   port: Number(SMTP_PORT),
-  // secure: true,
-  // auth: {
-  //   user: "REPLACE-WITH-YOUR-ALIAS@YOURDOMAIN.COM",
-  //   pass: "REPLACE-WITH-YOUR-GENERATED-PASSWORD",
-  // },
-})
+  secure: !!SMTP_SECURE,
+  auth: smtpAuth,
+});
 
 export async function sendAttachmentByEmail(
   fileBuffer: any,
   userEmailConfig: UserEmailConfig,
   configRecord: any
 ) {
-  const { from } = userEmailConfig
-  const { email: configEmail, fileKey } = configRecord
+  const { from } = userEmailConfig;
+  const { email: configEmail, fileKey } = configRecord;
 
-  const to = userEmailConfig.to || configEmail.to
-  const cc = userEmailConfig.cc || from
-  const subject = userEmailConfig.subject || configEmail.subject || "申請書"
-  const html = userEmailConfig.html || configEmail.html || "ご確認ください"
+  const to = userEmailConfig.to || configEmail.to;
+  const cc = userEmailConfig.cc || from;
+  const subject = userEmailConfig.subject || configEmail.subject || "申請書";
+  const html = userEmailConfig.html || configEmail.html || "ご確認ください";
 
-  if (!from) throw "Sender email not defined"
-  if (!to) throw "Recipient email not defined"
+  if (!from) throw "Sender email not defined";
+  if (!to) throw "Recipient email not defined";
 
-  console.log(`Sending email with subject ${subject} from ${from} to ${to}`)
+  console.log(`Sending email with subject ${subject} from ${from} to ${to}`);
 
   await transporter.sendMail({
     from,
@@ -43,5 +54,5 @@ export async function sendAttachmentByEmail(
         content: fileBuffer,
       },
     ],
-  })
+  });
 }
